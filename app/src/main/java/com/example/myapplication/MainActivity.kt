0b7c1371a63a0fd849caa001d8729003efa0a7f5
Venkat Venky <maddivenkatateja.23.csm@anits.edu.in>
2026-03-18 10:19:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnShow = findViewById<Button>(R.id.btnShow)
+
+        btnShow.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Student Details Displayed!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
